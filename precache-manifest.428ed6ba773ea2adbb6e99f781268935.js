ab8c11fd70771be421d27a02d148cb94b126a093
moibrahem98 <mo.ibrahiim98@gmail.com>
2022-06-13 21:39:21 +0300

    Updates

---
+++ precache-manifest.428ed6ba773ea2adbb6e99f781268935.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cb1c512b7036810c4fb83f5ac1cdd89",
+    "url": "/moibrahem98/react-market-basket/index.html"
+  },
+  {
+    "revision": "43fa14f600b998b84dd7",
+    "url": "/moibrahem98/react-market-basket/static/css/main.e6bb01d6.chunk.css"
+  },
+  {
+    "revision": "8dc858ad2f5199b0af5a",
+    "url": "/moibrahem98/react-market-basket/static/js/2.2486421f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moibrahem98/react-market-basket/static/js/2.2486421f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43fa14f600b998b84dd7",
+    "url": "/moibrahem98/react-market-basket/static/js/main.2f73ab18.chunk.js"
+  },
+  {
+    "revision": "5d7cf03a6b83dcb13ea0",
+    "url": "/moibrahem98/react-market-basket/static/js/runtime-main.feeeb43a.js"
+  }
+]);
